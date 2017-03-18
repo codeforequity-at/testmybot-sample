255f02ddcb62e7a7aadc61bcfbf55781daef0f5c
Florian Treml <ftreml@gmail.com>
2017-03-18 20:56:37 +0100

    run convos as test script

--- spec/testmybot.spec.js
+++ spec/testmybot.spec.js
@@ -1,7 +1,6 @@
-
-
+const bot = require('testmybot');
+  
 describe('TestMyBot Sample Conversation Test Suite', function() {
-  var bot = require('testmybot');
 
   beforeAll(function(done) {
     bot.beforeAll().then(done);
@@ -21,12 +20,23 @@ describe('TestMyBot Sample Conversation Test Suite', function() {
 
   it('should answer to uptime', function(done) {
     
-    bot.hears('uptime');
-    
-    bot.says().then((msg) => {
-      expect(msg.messageText).toMatch(/I have been running for/);
-      done();
-    });
-  });
-
+    bot.hears('uptime')
+      .then(() => bot.says())
+      .then((msg) => {
+        expect(msg.messageText).toMatch(/I have been running for/);
+        done();
+      })
+      .catch((err) => fail(err));
+  });  
+  
+  bot.setupTestSuite(
+    (testcaseName, testcaseFunction) => {
+      it(testcaseName, testcaseFunction, 60000);
+    },
+    (response, tomatch) => {
+      expect(response).toMatch(tomatch);
+    },
+    (err) => fail(err)
+  )
 });
+ 
